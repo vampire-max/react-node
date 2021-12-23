0109376569252939c416ccd7eabca3fa4dcdbedf
Naeem Ahmad <naeem.ahmad.9m@gmail.com>
2021-12-23 20:32:07 +0600

    udpated

--- src/components/user.js
+++ src/components/user.js
@@ -1,128 +1,164 @@
-import React, { useEffect, useState } from 'react'
-import { getApiClient } from '../module/axios'
+import React, { useEffect, useState } from "react";
+import { getApiClient } from "../module/axios";
 
-const UserForm = () => {
-  const [value, setValue] = useState({})
-  const [users, setUsers] = useState([])
-
-  const handleChange = (fieldname) => (e) => {
-    setValue((prevValue) => ({ ...prevValue, [fieldname]: e.target.value }))
-  }
-
-  const handleSubmit = () => {
-    console.log('values:', value)
-    getApiClient()
-      .post('/user/add-user', value)
-      .then((res) => {
-        console.log(res)
-        fetchUsers()
-      })
-      .catch((e) => {
-        console.log(e)
-      })
-  }
-
-  const fetchUsers = () => {
-    getApiClient()
-      .get('/user/getall')
-      .then((res) => {
-        console.log(res)
-        setUsers(res.data.users)
-      })
-      .catch((e) => console.log(e))
-  }
-
+const Users = () => {
+  const [users, setUsers] = useState([]);
   useEffect(() => {
-    
-    console.log('users', users)
-  }, [users])
+    getApiClient()
+      .get("/user/getall")
+      .then((res) => {
+        console.log(res);
+        setUsers(res.data.users);
+      })
+      .catch((e) => console.log(e));
+  }, []);
   return (
     <div>
-      <form>
-        <ul>
-          <li>
-            <label>
-              Name:
-              <input
-                type="text"
-                name="name"
-                value={value.name}
-                onChange={handleChange('name')}
-              />
-            </label>
-          </li>
-          <li>
-            <label>
-              userName:
-              <input
-                type="text"
-                name="username"
-                value={value.username}
-                onChange={handleChange('username')}
-              />
-            </label>
-          </li>
-          <li>
-            <label>
-              password:
-              <input
-                type="password"
-                name="password"
-                value={value.password}
-                onChange={handleChange('password')}
-              />
-            </label>
-          </li>
-          <li>
-            <label>
-              gender:
-              <input
-                type="text"
-                name="gender"
-                value={value.gender}
-                onChange={handleChange('gender')}
-              />
-            </label>
-          </li>
-          <li>
-            <label>
-              country:
-              <input
-                type="text"
-                name="country"
-                value={value.country}
-                onChange={handleChange('country')}
-              />
-            </label>
-          </li>
-        </ul>
-
-        <button type="button" onClick={handleSubmit}>
-          Submit User
-        </button>
-      </form>
+      <UserForm
+        onSuccess={(newUser) => setUsers((prev) => [newUser, ...prev])}
+      />
       <div>
         <table className="table">
-          <tr>
-            <th>name</th>
-            <th>username</th>
-            <th>password</th>
-            <th>gender</th>
-            <th>country</th>
-          </tr>
-          {users.map((user) => (
+          <thead>
             <tr>
-              <td key={user._id}>{user.name}</td>
-              <td key={user._id}>{user.username}</td>
-              <td key={user._id}>{user.password}</td>
-              <td key={user._id}>{user.gender}</td>
-              <td key={user._id}>{user.country}</td>
+              <th>name</th>
+              <th>username</th>
+              <th>password</th>
+              <th>gender</th>
+              <th>country</th>
+              <th>action</th>
             </tr>
-          ))}
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <tr key={user._id}>
+                <td>{user.name}</td>
+                <td>{user.username}</td>
+                <td>{user.password}</td>
+                <td>{user.gender}</td>
+                <td>{user.country}</td>
+                <td>
+                  <button
+                    onClick={() => {
+                      // use edit props to fill the form with this user data.
+                      // on handleSubmit, check if edit prop has data, if so
+                      // call update-user, or call add-user
+                    }}
+                  >
+                    Edit
+                  </button>
+                  <button
+                    onClick={() => {
+                      console.log(
+                        `Call delete api with id ${user._id}. then filter out this user.`
+                      );
+                    }}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
         </table>
       </div>
     </div>
-  )
-}
+  );
+};
+const UserForm = ({ edit, onSuccess }) => {
+  const [name, setName] = useState(edit?.name || "");
+  const [username, setUsername] = useState(edit?.username || "");
+  const [password, setPassword] = useState(edit?.password || "");
+  const [gender, setGender] = useState(edit?.gender || "");
+  const [country, setCountry] = useState(edit?.country || "");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getApiClient()
+      .post("/user/add-user", {
+        name,
+        username,
+        password,
+        gender,
+        country,
+      })
+      .then((res) => {
+        if (res.status === 200) {
+          onSuccess?.(res.data);
+          setName("");
+          setUsername("");
+          setPassword("");
+          setGender("");
+          setCountry("");
+        }
+        console.log(res);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <ul>
+        <li>
+          <label>
+            Name:
+            <input
+              type="text"
+              name="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+        </li>
+        <li>
+          <label>
+            userName:
+            <input
+              type="text"
+              name="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </label>
+        </li>
+        <li>
+          <label>
+            password:
+            <input
+              type="password"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>
+        </li>
+        <li>
+          <label>
+            gender:
+            <input
+              type="text"
+              name="gender"
+              value={gender}
+              onChange={(e) => setGender(e.target.value)}
+            />
+          </label>
+        </li>
+        <li>
+          <label>
+            country:
+            <input
+              type="text"
+              name="country"
+              value={country}
+              onChange={(e) => setCountry(e.target.value)}
+            />
+          </label>
+        </li>
+      </ul>
 
-export default UserForm
+      <button>Submit User</button>
+    </form>
+  );
+};
+
+export default Users;
