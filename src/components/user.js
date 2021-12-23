701cc28bf31a89fa289c99afc5f2c13de6554208
jana6600 <jananikolic6600@gmail.com>
2021-12-23 04:27:06 -0800

    post and get

--- src/components/user.js
+++ src/components/user.js
@@ -33,61 +33,94 @@ const UserForm = () => {
   }
 
   useEffect(() => {
-    // console.log('value', value)
-  }, [value])
+    console.log('users', users)
+  }, [users])
   return (
-    <form>
-      <ul>
-        <li>
-          <label>
-            Name:
-            <input
-              type="text"
-              name="name"
-              value={value.name}
-              onChange={handleChange('name')}
-            />
-          </label>
-        </li>
-        <li>
-          <label>
-            userName:
-            <input
-              type="text"
-              name="username"
-              value={value.username}
-              onChange={handleChange('username')}
-            />
-          </label>
-        </li>
-        <li>
-          <label>
-            password:
-            <input
-              type="password"
-              name="password"
-              value={value.password}
-              onChange={handleChange('password')}
-            />
-          </label>
-        </li>
-        <li>
-          <label>
-            gender:
-            <input
-              type="text"
-              name="gender"
-              value={value.gender}
-              onChange={handleChange('gender')}
-            />
-          </label>
-        </li>
-      </ul>
+    <div>
+      <form>
+        <ul>
+          <li>
+            <label>
+              Name:
+              <input
+                type="text"
+                name="name"
+                value={value.name}
+                onChange={handleChange('name')}
+              />
+            </label>
+          </li>
+          <li>
+            <label>
+              userName:
+              <input
+                type="text"
+                name="username"
+                value={value.username}
+                onChange={handleChange('username')}
+              />
+            </label>
+          </li>
+          <li>
+            <label>
+              password:
+              <input
+                type="password"
+                name="password"
+                value={value.password}
+                onChange={handleChange('password')}
+              />
+            </label>
+          </li>
+          <li>
+            <label>
+              gender:
+              <input
+                type="text"
+                name="gender"
+                value={value.gender}
+                onChange={handleChange('gender')}
+              />
+            </label>
+          </li>
+          <li>
+            <label>
+              country:
+              <input
+                type="text"
+                name="country"
+                value={value.country}
+                onChange={handleChange('country')}
+              />
+            </label>
+          </li>
+        </ul>
 
-      <button type="button" onClick={handleSubmit}>
-        Submit User
-      </button>
-    </form>
+        <button type="button" onClick={handleSubmit}>
+          Submit User
+        </button>
+      </form>
+      <div>
+        <table className="table">
+          <tr>
+            <th>name</th>
+            <th>username</th>
+            <th>password</th>
+            <th>gender</th>
+            <th>country</th>
+          </tr>
+          {users.map((user) => (
+            <tr>
+              <td key={user._id}>{user.name}</td>
+              <td key={user._id}>{user.username}</td>
+              <td key={user._id}>{user.password}</td>
+              <td key={user._id}>{user.gender}</td>
+              <td key={user._id}>{user.country}</td>
+            </tr>
+          ))}
+        </table>
+      </div>
+    </div>
   )
 }
 
